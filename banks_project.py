749703912c9b5c44a88b2bb1f72f40d2d4f7943e
proywanat <proywanat@gmail.com>
2025-12-01 22:27:41 +0700

    firsr commit: elt_project

---
+++ banks_project.py
@@ -0,0 +1,82 @@
+import pandas as pd 
+import numpy as np 
+import requests
+import sqlite3
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+def log_progress(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open('./code_log.txt','a') as f:
+        f.write(timestamp + ' : ' + message + '\n')
+
+def extract(url, table_attribs):
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    data_list = []
+
+    tables = data.find_all('tbody')
+    rows = tables[0].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            data_list.append({
+                'Name' : col[1].get_text(strip=True),
+                'MC_USD_Billion' : float(col[2].get_text(strip=True)),
+            })
+            df = pd.DataFrame(data_list, columns=table_attribs)
+    return df
+
+def transform(df, csv_path):
+    df1 = pd.read_csv(csv_path)
+    exchange_rate = df1.set_index('Currency')['Rate'].to_dict()
+
+    # transformed exchange rate
+    df['MC_GBP_Billion'] = [np.round(x*exchange_rate['GBP'],2) for x in df['MC_USD_Billion']]
+    df['MC_EUR_Billion'] = [np.round(x*exchange_rate['EUR'],2) for x in df['MC_USD_Billion']]
+    df['MC_INR_Billion'] = [np.round(x*exchange_rate['INR'],2) for x in df['MC_USD_Billion']]
+
+    return df
+
+def load_to_csv(df, output_path):
+    df.to_csv(output_path)
+
+def load_to_db(df, sql_connection, table_name):
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+
+def run_query(query_statement, sql_connection):
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+
+url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
+table_attribs = ['Name','MC_USD_Billion']
+csv_path = './exchange_rate.csv'
+output_path = './Largest_banks_data.csv'
+table_name = 'Largest_banks'
+db_name = 'Banks.db'
+
+log_progress('Preliminaries complete. Initiating ETL process')
+df = extract(url, table_attribs)
+log_progress('Data extraction complete. Initiating Transformation process')
+df = transform(df, csv_path)
+log_progress('Data transformation complete. Initiating Loading process')
+load_to_csv(df, output_path)
+log_progress('Data saved to CSV file')
+sql_connection = sqlite3.connect('Banks.db')
+log_progress('SQL Connection initiated')
+load_to_db(df, sql_connection, table_name)
+log_progress('Data loaded to Database as a table, Executing queries')
+
+query_statement = f"SELECT * FROM Largest_banks"
+run_query(query_statement, sql_connection)
+query_statement = f"SELECT AVG(MC_GBP_Billion) FROM Largest_banks"
+run_query(query_statement, sql_connection)
+query_statement = f"SELECT Name from Largest_banks LIMIT 5"
+run_query(query_statement, sql_connection)
+log_progress('Process Complete')
+
+sql_connection.close()
+log_progress('Server Connection closed')
